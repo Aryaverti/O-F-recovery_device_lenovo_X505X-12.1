fecba3542679ae0f414e5d53b6db9862ea556262
Aryaverti <tg967591@gmail.com>
2026-02-04 01:30:13 +0530

    Remove common_tablet.mk inheritance
    Removed common_tablet.mk inheritance from device makefile.

--- twrp_X505X.mk
+++ twrp_X505X.mk
@@ -20,7 +20,6 @@ PRODUCT_RELEASE_NAME := X505X
 
 # inherit the usual stuff...
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-$(call inherit-product, vendor/omni/config/common_tablet.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := twrp_X505X
